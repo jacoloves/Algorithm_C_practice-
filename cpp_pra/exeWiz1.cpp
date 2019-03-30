0ba8a47cbe036206af858a72d8fe989ba6094f38
jacobass <5511068t@gmail.com>
2019-03-30 21:36:53 +0900

    atcoder

---
+++ cpp_pra/exeWiz1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int A, B, C;
+
+    cin >> A >> B >> C;
+
+    if (A == B && B == C) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
---
+++ cpp_pra/exeWiz2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    char str;
+
+    cin >> str;
+
+    int cntR = 0;
+    int cntB = 0;
+
+    
+
+    if (cntR > cntB) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+} 
