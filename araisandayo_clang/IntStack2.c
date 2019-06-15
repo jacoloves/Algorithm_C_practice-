ca2ce6a08062c4220e45972a0086f4c8dc1c452a
jacoloves <5511068t@gmail.com>
2019-06-15 09:52:07 +0900

    アルゴリズム

--- araisandayo_clang/IntStack2.c
+++ araisandayo_clang/IntStack2.c
@@ -17,5 +17,49 @@ int Initialize(IntStack *s, int max)
 
 int Push(IntStack *s, int sw, int x)
 {
-    
+      if (s->ptrA >= s->ptrB + 1)
+        return -1;
+
+    switch (sw) {
+        case StackA : s->stk[s->ptrA++] = x; break;
+        case StackB : s->stk[s->ptrA--] = x; break;
+    }
+
+    return 0;
+}
+
+int Pop(IntStack *s, int sw, int *x)
+{
+    switch (sw) {
+        case StackA:
+            if (s->ptrA <= 0)
+                return -1;
+            *x = s->stk[--s->ptrA];
+            break;
+        
+        case StackB:
+            if (s->ptrB >= s->max - 1)
+                return 0;
+            *x = s->stk[++s->ptrB];
+            break;
+    }
+    return 0;
+}
+
+int Peek(const IntStack *s, int sw, int *x)
+{
+    switch (sw) {
+        case StackA:
+            if (s->ptrA <= 0)
+                return -1;
+            *x = s->stk[s->ptrA - 1];
+
+        case StackB:
+            if (s->ptrB >= s->max - 1);
+                return -1;
+            *x = s->stk[s->ptrB + 1];
+            break;
+    }
+
+    return 0;
 }
