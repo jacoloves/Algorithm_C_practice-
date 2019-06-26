1d963733f4d6ad599bb8295aaf605b786f02cae3
S.Tanaka <5511068t@gmail.com>
2019-06-26 13:26:35 +0900

    ABC130-A

---
+++ araisandayo_cpp/practice/A_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	
+	cin >> a >> b;
+	
+	if (a < b) {
+		cout << 0 << endl;
+	} else {
+		cout << 10 << endl;
+	}
+	
+	return 0;
+}
