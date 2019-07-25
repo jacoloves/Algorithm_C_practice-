e1d722e718bcf4e2bbb13188bf22db367af888b1
jacoloves <5511068t@gmail.com>
2019-07-26 02:48:19 +0900

    アルゴリズム

--- araisandayo_cpp/practice/C_3.cpp
+++ araisandayo_cpp/practice/C_3.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
 
+using namespace std;
+
+typedef pair<int, int> P;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> data(N);
+    vector<int> data2(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+
+    sort(b.begin(), b.end(), greater<int>());
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == b[i]) {
+            cout << b[1] << endl; 
+        } else {
+            cout << b[0] << endl;
+        }
+    }
+
+}
 
