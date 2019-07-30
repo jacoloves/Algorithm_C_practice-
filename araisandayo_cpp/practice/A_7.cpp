fbffa1049f3f4e2b2987c614af5f9bdeb259c897
jacoloves <5511068t@gmail.com>
2019-07-31 00:42:42 +0900

    アルゴリズム

--- araisandayo_cpp/practice/A_7.cpp
+++ araisandayo_cpp/practice/A_7.cpp
@@ -1,1 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int ans, s, d;
+    
+    ans = abs(a-b);
+ 
+    ans = ans/2;
+
+    if (a > b) {
+        s = a - ans;
+        d = b + ans;
+    } else {
+        s = b - ans;
+        d = a + ans;
+    }
+
+    if (s == d) {
+        cout << s << endl;
+    } else {
+        cout << "IMPOSSIBLE" << endl;
+    }
+
+    
+
+    return 0;
+    
+}
