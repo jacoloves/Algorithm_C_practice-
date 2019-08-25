8f99e88e32f6e341c22ec3b4bdd7fdd6b1c39588
jacoloves <5511068t@gmail.com>
2019-08-25 21:27:46 +0900

    Atcoder

--- araisandayo_cpp/practice/b.cpp
+++ araisandayo_cpp/practice/b.cpp
@@ -9,45 +9,43 @@ typedef long long ll;
 using namespace std;
 
 int main() {
-    int N, cnt = 0, k_cnt = 1;
+    int N, N2, cnt = 0, k_cnt = 1;
 
     cin >> N;
 
+    N2 = N;
+
     while(1) {
+        if (N2 < 10) {
+            for (int i = 1; i <= N2; i++) {
+                cnt++;
+            }
+            break;
+        }
+
+        if (k_cnt == 1) {
+            for (int i = 1; i <= 9; i++) {
+                cnt++;
+            }
+        } else if (k_cnt == 3) {
+            for (int i = 100; i <= 999; i++) {
+                cnt++;
+            }
+         } if (k_cnt == 5) {
+            for (int i = 10000; i <= 99999; i++) {
+                cnt++;
+            }
+         }
+
         if (N / 10 == 0) {
             break;
         }
+        N = N / 10;
+
         k_cnt++;
-
-    if (k_cnt == 1) {
-        for (int i = 1; i <= 9; i++) {
-            cnt++;
-        }
-    } else if (k_cnt == 3) {
-        for (int i = 100; i <= 999; i++) {
-            cnt++;
-        }
-    } if (k_cnt == 5) {
-        for (int i = 10000; i <= 99999; i++) {
-            cnt++;
-        }
     }
 
-    }
-
-    if (k_cnt == 1) {
-        for (int i = 1; i <= 9; i++) {
-            cnt++;
-        }
-    } else if (k_cnt == 3) {
-        for (int i = 100; i <= 999; i++) {
-            cnt++;
-        }
-    } if (k_cnt == 5) {
-        for (int i = 10000; i <= 99999; i++) {
-            cnt++;
-        }
-    }
+    cout << cnt << endl;
 
     return 0;
 }
