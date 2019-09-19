4d1218d6c21787081026f885b512233545a048b2
jacoloves <5511068t@gmail.com>
2019-09-20 06:39:14 +0900

    アルゴリズム

---
+++ araisandayo_cpp/practice/a4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    int n, ans = 0;
+	cin >> n;
+	
+	ans = pow(n, 3.0);
+	
+	cout << ans << endl;
+	
+	return 0;
+}
