c386231d462a7ba1393b10f3c3961627a5ad79fc
jacoloves <5511068t@gmail.com>
2019-12-03 06:55:02 +0900

    20191203

--- araisandayo_cpp/practice/c1.cpp
+++ araisandayo_cpp/practice/c1.cpp
@@ -9,35 +9,9 @@ typedef long long ll;
 
 using namespace std;
 
-int main() {
-    int a;
-	cin >> a;
+int main(void)
+{
+    printf("test\n");
 
-	vector<int> data1(a);
-	vector<int> data2(a);
-	vector<int> data3(a);
-
-	for (int i = 0; i < a; i++) {
-		data1[i] = i+1;
-	}
-
-	for (int i = 0; i < a; i++) {
-		cin >> data2[i];
-	}
-
-	for (int i = 0; i < a; i++) {
-		for (int j = 0; j < a; j++) {
-			if (data1[i] == data2[j]) {
-				data3[i] = data1[j];
-			}
-		}
-	}
-
-	for (int i = 0; i < a; i++) {
-		cout << data3[i];
-		cout << " ";				
-	}
-
-	cout << endl;
-
+    return 0;
 }
--- araisandayo_cpp/practice/smp.cpp
+++ araisandayo_cpp/practice/smp.cpp
@@ -1,4 +1,13 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+
+typedef long long ll;
+
+using namespace std;
 
 int main(void)
 {
