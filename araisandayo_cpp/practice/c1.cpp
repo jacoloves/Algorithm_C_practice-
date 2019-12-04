ce0a8e651e289f15b93c3bd4c12adda4a401e74e
jacoloves <5511068t@gmail.com>
2019-12-04 23:39:22 +0900

    20191204

--- araisandayo_cpp/practice/c1.cpp
+++ araisandayo_cpp/practice/c1.cpp
@@ -9,9 +9,28 @@ typedef long long ll;
 
 using namespace std;
 
-int main(void)
-{
-    printf("test\n");
+int N, X[8], Y[8];
+//---------------------------------------------------------------------------------------------------
+void _main() {
+    cin >> N;
+    rep(i, 0, N) cin >> X[i] >> Y[i];
 
-    return 0;
+    vector<int> ord;
+    rep(i, 0, N) ord.push_back(i);
+
+    long double sm = 0;
+    do {
+        rep(i, 0, N - 1) {
+            int a = ord[i];
+            int b = ord[i + 1];
+
+            long double dx = X[a] - X[b];
+            long double dy = Y[a] - Y[b];
+
+            sm += sqrt(dx * dx + dy * dy);
+        }
+    } while (next_permutation(all(ord)));
+
+    rep(i, 0, N) sm /= (i + 1);
+    printf("%.10Lf\n", sm);
 }
