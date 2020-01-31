210f1aa5432cf1cf42dd4f7f705324975312bff9
S.Tanaka <5511068t@gmail.com>
2020-01-31 13:11:31 +0900

    20200131

---
+++ araisandayo_cpp/practice/a_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(void)
+{
+	char a;
+	cin >> a;
+
+	cout << char(a+1) << endl;
+
+	return 0;
+}
