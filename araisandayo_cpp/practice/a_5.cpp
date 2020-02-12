0bec9c8bd42302581338d7e5a609f9959cb701f0
S.Tanaka <5511068t@gmail.com>
2020-02-12 11:35:29 +0900

    20200212

---
+++ araisandayo_cpp/practice/a_5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(void)
+{
+	string a, b;
+	cin >> a >> b;
+	string c = b + a;
+
+	cout << c << endl;
+
+	return 0;
+}
